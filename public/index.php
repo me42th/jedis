9afa40af4de2b5a552d9f11844b1710e81a4e753
David Meth <26856017+me42th@users.noreply.github.com>
2025-03-22 15:04:23 -0300

    Revert commit

--- public/index.php
+++ public/index.php
@@ -1,22 +1,3 @@
 <?php
 
-$recipe = 'Bolo de Chocolate:
-
-Ingredientes:
-- 2 xícaras de farinha de trigo
-- 1 xícara de açúcar
-- 1/2 xícara de chocolate em pó
-- 1 colher de sopa de fermento
-- 1 xícara de leite
-- 1/2 xícara de óleo
-- 2 ovos
-
-Modo de Preparo:
-1. Misture todos os ingredientes secos em uma tigela.
-2. Adicione o leite, óleo e ovos e misture bem.
-3. Despeje em uma forma untada e leve ao forno pré-aquecido a 180°C por 30 minutos.
-4. Espere esfriar e bom apetite!';
-
-echo $recipe;
-
-?>
+$recipe = "Receita de bolo: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
