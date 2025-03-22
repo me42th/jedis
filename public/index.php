b57b2be426a7830ae447b781c0ccd63058f7f133
David Meth <26856017+me42th@users.noreply.github.com>
2025-03-22 15:24:29 -0300

    Reverted public/index.php to Laravel default

--- public/index.php
+++ public/index.php
@@ -1,28 +1,38 @@
 <?php
 
-$recipe = 'Receita de Bolo:
+/*
+|--------------------------------------------------------------------------
+| Bootstrap The Script
+|--------------------------------------------------------------------------
+|
+| This script maps the console commands to the application and
+| routes them to the correct controller method. You may
+| customize this process very easily by simply extending
+| this bootstrap class and importing the proper routes.
+|
+*/
 
-Ingredientes:
-- 2 xícaras de farinha de trigo
-- 1 xícara de açúcar
-- 3 ovos
-- 1 colher de fermento em pó
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
-Modo de Preparo:
-1. Misture todos os ingredientes em uma tigela.
-2. Despeje a massa em uma forma untada.
-3. Leve ao forno preaquecido por 40 minutos.
-4. Sirva e aproveite!';
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
 
-?>
+require_once __DIR__.'/../vendor/autoload.php';
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Receita de Bolo</title>
-</head>
-<body>
-    <h1>Receita de Bolo</h1>
-    <p><?php echo $recipe; ?></p>
-</body>
-</html>
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
