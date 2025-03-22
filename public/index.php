761b2c9972193cf7c3e78a1ec8a4e3862639c76c
David Meth <26856017+me42th@users.noreply.github.com>
2025-03-22 15:07:07 -0300

    Revert to default Laravel index.php file

--- public/index.php
+++ public/index.php
@@ -1,3 +1,31 @@
 <?php
 
-$recipe = "Receita de bolo: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+/*
+|--------------------------------------------------------------------------
+| Web Application
+|--------------------------------------------------------------------------
+|
+| Este arquivo serve para carregar o aplicativo web e responder a solicitações.
+| Você pode personalizar este arquivo conforme necessário para o seu aplicativo.
+|
+*/
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
